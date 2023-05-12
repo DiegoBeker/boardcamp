0fb063fc615a8f8d1503b925954404b65c4d7129
Diego <diegobecker2007@hotmail.com>
2023-05-12 12:26:55 -0300

    feat: add POST and GET methods to /games route

---
+++ src/controllers/games.controller.js
@@ -0,0 +1,23 @@
+import { db } from "../database/database.connection.js";
+
+export async function getGames(req, res) {
+    try {
+        const games = await db.query(`SELECT * FROM games`);
+        res.send(games.rows);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+export async function postGame(req, res) {
+    const { name, image, stockTotal, pricePerDay } = req.body;
+
+    const nameExists = await db.query(`SELECT * FROM games WHERE name=$1`, [name]);
+    if (nameExists.rows[0]) return res.status(409).send("Game already exists");
+
+    await db.query(`
+        INSERT INTO games (name, image, "stockTotal", "pricePerDay")
+        VALUES ($1, $2, $3, $4);
+    `, [name, image, stockTotal, pricePerDay]);
+    res.sendStatus(201);
+}
