51fd9440ede4a7ba528a3e9d6db1854fb44d8f60
Diego <diegobecker2007@hotmail.com>
2023-05-16 10:34:41 -0300

    fix: correct customer schema

--- src/schemas/customer.schema.js
+++ src/schemas/customer.schema.js
@@ -1,9 +1,8 @@
 import joi from "joi";
 
 export const customerSchema = joi.object({
-    name: joi.string().required(),
-    phone: joi.string().required(),
-    cpf: joi.string().min(10).max(11).pattern(/^[0-9]+$/).required(),
-    birthday: joi.string().pattern(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/
-    ).required()
+    name: joi.string().trim().required(),
+    phone: joi.string().min(10).max(11).pattern(/^[0-9]+$/).required(),
+    cpf: joi.string().length(11).pattern(/^[0-9]+$/).required(),
+    birthday: joi.string().pattern(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/).required()
 });
